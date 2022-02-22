3b385c607ffdd2afea41e7f155b0955b487c544e
Igo vieira <igo.vieira@live.com>
2022-02-22 08:33:51 -0300

    ...

--- testt.js
+++ testt.js
@@ -1,3 +1,31 @@
+
+/*function cliclou(){
+    var titulo = document.querySelector("#texto").value
+    var botao1 = document.querySelector("#botao").value
+    var caixa = document.querySelector('#pp')
+    caixa.innerText = titulo
+}*/
+
+/*var strLista = "igo";
+
+var listaSubstituicao = {
+    a:"ai",
+    e:"enter",
+    i:"imes",
+    o:"ober",
+    u:"ufat"
+}
+
+
+alteracaoLista = strLista.replace(/a|e|i|o|u/gi, function(item){
+    let it = listaSubstituicao[item];
+    let itemLista = it.replace(/(?:^|\s)\S/g, function(elemento) { return elemento.toUpperCase(); });
+    return itemLista;
+});
+console.log(alteracaoLista);*/
+
+
+
 function loadpk(){
     let numero1 = document.getElementById('numero1').value
     fetch(`https://pokeapi.co/api/v2/pokemon/${numero1}`)
@@ -34,7 +62,6 @@ function loadpk(){
 
 
 document.getElementById('btn1').onclick = loadpk
-
 
 
 
