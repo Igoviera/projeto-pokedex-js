6091e5ff94fa94831e22779461c2d3bbf85aad0f
Igo vieira <igo.vieira@live.com>
2022-02-22 08:25:00 -0300

    Atualização

--- testt.js
+++ testt.js
@@ -1,33 +1,40 @@
 function loadpk(){
     let numero1 = document.getElementById('numero1').value
-    url = `https://pokeapi.co/api/v2/pokemon/${numero1}`
-    fetch(url)
-    .then((response) => {return response.json()})
+    fetch(`https://pokeapi.co/api/v2/pokemon/${numero1}`)
+    .then((response) => response.json())
 
     .then((data) =>{
-        document.getElementById('nome').innerHTML = data['name']
-        document.getElementById('pic').setAttribute('src',data.sprites.other.dream_world.front_default)
-    })
 
+        let statName = ''
+        let baseStat = ''
+        data.stats.forEach(element => {
+            statName += element.stat.name+'<br>'
+            baseStat +=  element.base_stat+'<br>'
+            document.getElementById('hp').innerHTML = statName
+            document.getElementById('valor').innerHTML = baseStat
+          
+        });
+
+        
+           
+        document.getElementById('nome').innerHTML = data.name
+        document.getElementById('pic').setAttribute('src',data.sprites.other.dream_world.front_default)
+   
+
+
+        const carat = data.map(data => data.stats)
+        console.log(carat)
+
+    })    
     .catch((erro) => {
         console.log('erro:'+ erro)
     })
 }
 
-/*var contador = 0
 
-function increment(){
-    updateDisplay(++contador)
-}
-function dessencremento(){
-    updateDisplay(--contador)
-}
-
-function updateDisplay(val){
-    document.getElementById("counter-label").innerHTML = val
-}*/
 
 document.getElementById('btn1').onclick = loadpk
+
 
 
 
